8a9f37f17bee4554c8d8fae9f46ba92b57f552ae
sara-patino <sara.patino@alumnos.uneatlantico.es>
2025-03-08 20:44:33 +0100

    update cart

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     return localStorageCart ? JSON.parse(localStorageCart) : []
   }
 
-  const [data, setData] = useState(db);
+  const [data] = useState(db);
   const [cart, setCart] = useState(initialCart);
 
   const MAX_ITEMS =5
